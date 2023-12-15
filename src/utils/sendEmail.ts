5d5cf68febe2c9742f0fd793c11b718104b79c88
a7mad1112 <ahmalawneh79@gmail.com>
2023-12-15 20:14:25 +0200

    implement sendEmail utility

---
+++ src/utils/sendEmail.ts
@@ -0,0 +1,22 @@
+import nodemailer, { SendMailOptions } from 'nodemailer';
+
+export async function sendEmail(to: string, subject: string, html: any) {
+  const { NODELAILER_PASS, NODEMAILER_EMAIL } = process.env;
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: NODEMAILER_EMAIL,
+      pass: NODELAILER_PASS,
+    },
+  });
+
+  const mailOptions: SendMailOptions = {
+    from: 'Tellonym_Spam',
+    to,
+    subject,
+    html,
+  };
+  const info = await transporter.sendMail(mailOptions);
+  console.log('1234');
+  return info;
+}
